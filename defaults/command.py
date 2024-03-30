137d375f26b5a91f2855f2501b6bab5a362ed17f
mogryo <tad.baikstis@gmail.com>
2024-03-30 11:37:19 +0200

    Remove variable re-declaration

--- defaults/command.py
+++ defaults/command.py
@@ -8,11 +8,6 @@ DEFAULT_COLUMNS = ",".join([
     CliTableColumn.COMMIT_AMOUNT.value
 ])
 
-DEFAULT_SORT = [
-    SortingRule(CliTableColumn.DELETED_ADDED_RATIO, SortingDirection.DESC),
-    SortingRule(CliTableColumn.LINE_COUNT, SortingDirection.DESC),
-]
-
 DEFAULT_SORT = ",".join([
     f"{CliTableColumn.DELETED_ADDED_RATIO.value}-{SortingDirection.DESC.value}",
     f"{CliTableColumn.LINE_COUNT.value}-{SortingDirection.DESC.value}",
