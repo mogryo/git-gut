4aad93481ca12b61e727405e2b394a5e29bc988c
mogryo <tad.baikstis@gmail.com>
2024-03-31 11:52:44 +0300

    Change the unique key in draw table

--- utils/cli_table.py
+++ utils/cli_table.py
@@ -20,8 +20,8 @@ def draw_flat_tree_table(
     pretty_table.field_names = [CliTableColumnLabel.ID.value.upper()] + list(
         map(lambda x: CliTableColumnLabel[x.value.upper()].value, column_technical_names)
     )
-    ids = generate_unique_keys(start_key=1, end_key=len(rows), key_length=len(str(len(rows))))
-    for index, row in enumerate(rows):
-        pretty_table.add_row([ids[index], *row])
+    unique_keys = generate_unique_keys(start_key=1, end_key=len(rows), key_length=len(str(len(rows))))
+    for unique_key, row in zip(unique_keys, rows):
+        pretty_table.add_row([unique_key, *row])
 
     print(pretty_table)
