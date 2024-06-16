9526ccfd9e88ca8596973f38b458054fafc0e41d
mogryo <tad.baikstis@gmail.com>
2024-06-16 12:55:54 +0300

    Correct help messages

--- query_option_parser/string_tokens.py
+++ query_option_parser/string_tokens.py
@@ -1,7 +1,7 @@
 """String tokens: signs, condition join words and etc."""
 
-NUMBER_SIGNS = {">", "<", ">=", "<=", "="}
-TEXT_SIGNS = {"="}
+NUMBER_SIGNS = {">", "<", ">=", "<=", "==", "!="}
+TEXT_SIGNS = {"=="}
 ALLOWED_SIGNS = NUMBER_SIGNS.union(TEXT_SIGNS)
 CONDITION_JOIN = {"and"}
 TOP_LEVEL_STATEMENT_KEYWORDS = {"SHOW", "FROM", "WHERE", "ORDERBY"}
