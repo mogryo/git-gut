59cfb1e39c3add94b861e362e3533f67a56cb672
mogryo <tad.baikstis@gmail.com>
2024-09-30 20:39:19 +0300

    Adjust test file according pylint

--- utils/tests/test_parse_separate_options_into_query.py
+++ utils/tests/test_parse_separate_options_into_query.py
@@ -4,35 +4,35 @@ from app_types.dataclasses import SeparateOptionsAsQuery
 from enums.user_input_keywords import QueryKeywords
 from utils.command_option_parser import parse_separate_options_into_query
 
-columns = "linecount, daratio"
-file_path = "./utils"
-filters = "linecount > 100 AND daratio < 10"
-sort = "linecount ASC AND daratio DESC"
+COLUMNS = "linecount, daratio"
+FILE_PATH = "./utils"
+FILTERS = "linecount > 100 AND daratio < 10"
+SORT = "linecount ASC AND daratio DESC"
 
 
 def test_only_columns() -> None:
     """Check when only columns are provided"""
-    result = parse_separate_options_into_query(SeparateOptionsAsQuery(columns=columns))
-    assert result == f"{QueryKeywords.SHOW.value} {columns}"
+    result = parse_separate_options_into_query(SeparateOptionsAsQuery(columns=COLUMNS))
+    assert result == f"{QueryKeywords.SHOW.value} {COLUMNS}"
 
 
 def test_columns_path() -> None:
     """Check when columns and path are provided"""
     result = parse_separate_options_into_query(
-        SeparateOptionsAsQuery(columns=columns, file_path=file_path)
+        SeparateOptionsAsQuery(columns=COLUMNS, file_path=FILE_PATH)
     )
     assert (
         result
-        == f"{QueryKeywords.SHOW.value} {columns} {QueryKeywords.FROM.value} {file_path}"
+        == f"{QueryKeywords.SHOW.value} {COLUMNS} {QueryKeywords.FROM.value} {FILE_PATH}"
     )
 
 
 def test_orderby_and_where() -> None:
     """Check when sort and filters are provided"""
     result = parse_separate_options_into_query(
-        SeparateOptionsAsQuery(filters=filters, sort=sort)
+        SeparateOptionsAsQuery(filters=FILTERS, sort=SORT)
     )
     assert (
         result
-        == f"{QueryKeywords.WHERE.value} {filters} {QueryKeywords.ORDERBY.value} {sort}"
+        == f"{QueryKeywords.WHERE.value} {FILTERS} {QueryKeywords.ORDERBY.value} {SORT}"
     )
